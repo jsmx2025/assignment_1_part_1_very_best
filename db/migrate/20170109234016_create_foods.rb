b0d958b0220bae87506f2bf933e2699f00090155
Raghu Betina <raghu@firstdraft.com>
2017-01-09 23:40:16 +0000

    Generate foods resource with starter:resource

---
+++ db/migrate/20170109234016_create_foods.rb
@@ -0,0 +1,12 @@
+class CreateFoods < ActiveRecord::Migration
+  def change
+    create_table :foods do |t|
+      t.string :dish
+      t.integer :cuisine_id
+      t.integer :venue_id
+
+      t.timestamps
+
+    end
+  end
+end
